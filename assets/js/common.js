69b330553aa05b839fe5494facc25f90944b3558
AymaneMAZZ <aymanemazzour@gmail.com>
2025-01-19 23:23:27 +0100

    final

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,143 @@
-/*=============== Show Menu =============== */
+(() => {
+    /*=============== Show Menu =============== */
+    const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+  
+    /*===== Menu Show =====*/
+    if (navToggle) {
+      navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+      });
+    }
+  
+    /*===== Hide Show =====*/
+    if (navClose) {
+      navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+      });
+    }
+  
+    /*=============== Remove Menu Mobile =============== */
+    const navLink = document.querySelectorAll('.nav__link');
+    function linkAction() {
+      navMenu.classList.remove('show-menu');
+    }
+    navLink.forEach((n) => n.addEventListener('click', linkAction));
+  
+    /*=============== Background Header =============== */
+    function scrollHeader() {
+      const header = document.getElementById('header');
+      if (window.scrollY >= 50) header.classList.add('scroll-header');
+      else header.classList.remove('scroll-header');
+    }
+    window.addEventListener('scroll', scrollHeader);
+  
+    /*=============== Contact Form =============== */
+    const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      message = document.getElementById('message'),
+      contactMessage = document.getElementById('contact-message');
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+      if (
+        contactName.value.trim() === '' ||
+        contactEmail.value.trim() === '' ||
+        message.value.trim() === ''
+      ) {
+        contactMessage.classList.remove('color-light');
+        contactMessage.classList.add('color-dark');
+        contactMessage.textContent = 'Write all the input fields';
+        return;
+      }
+      emailjs
+        .sendForm(
+          'service_u2836qq',
+          'template_ynpcmlk',
+          '#contact-form',
+          'OxNY-YSN63VeSWqn-'
+        )
+        .then(
+          () => {
+            contactMessage.classList.remove('color-dark');
+            contactMessage.classList.add('color-light');
+            contactMessage.textContent = 'Message sent ✔️';
+            contactForm.reset();
+            setTimeout(() => {
+              contactMessage.textContent = '';
+            }, 5000);
+          },
+          (error) => {
+            contactMessage.classList.remove('color-light');
+            contactMessage.classList.add('color-dark');
+            contactMessage.textContent = 'Message sending failed';
+            console.error('Error:', error);
+          }
+        );
+    };
+    contactForm.addEventListener('submit', sendEmail);
+    
+    /*=============== Style Switcher =============== */
+    const styleSwitcherToggle = document.querySelector('.style__switcher-toggler'),
+    styleSwitcher = document.querySelector('.style__switcher');
+    styleSwitcherToggle.addEventListener('click', ()=> {
+        styleSwitcher.classList.toggle('open');
+    });
+    //hide switcher on scroll
+    window.addEventListener('scroll',()=>{
+        if (styleSwitcher.classList.contains('open')) {
+            styleSwitcher.classList.remove('open');
+            
+        }
+    })
+    const alternateStyles = document.querySelectorAll('.alternate-style');
 
-/*===== Menu Show =====*/
-/* Validate if constant exists */
+//    function setActiveStyle(color){
+//     alternateStyles.forEach((style) => {
+//         if(color === style.getAttribute('title')){
+//             style.removeAttribute('disabled');
+//         }else{
+//             style.setAttribute('disabled','true');
+//         }
+//     });
+//    }
 
-/*===== Hide Show =====*/
-/* Validate if constant exists */
 
-/*=============== Remove Menu Mobile =============== */
+    // function setActiveStyle(color) {
+    //     alternateStyles.forEach((style) => {
+    //         if (color === style.getAttribute('title')) {
+    //             style.removeAttribute('disabled');
+    //             localStorage.setItem('preferredStyle', color); // Sauvegarde
+    //         } else {
+    //             style.setAttribute('disabled', 'true');
+    //         }
+    //     });
+    // }
 
-/*=============== Background Header =============== */
+    window.setActiveStyle = function (color) {
+        alternateStyles.forEach((style) => {
+            if (color === style.getAttribute('title')) {
+                style.removeAttribute('disabled');
+            } else {
+                style.setAttribute('disabled', 'true');
+            }
+        });
+    };
+    
 
-/*=============== Contact Form =============== */
+    // Appliquer le style enregistré au chargement
+    document.addEventListener('DOMContentLoaded', () => {
+        const savedStyle = localStorage.getItem('preferredStyle');
+        if (savedStyle) {
+            setActiveStyle(savedStyle);
+        }
+    });
 
-/*=============== Style Switcher =============== */
+
+
+
+
+  })();
+  
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 /*=============== Active Link =============== */
+const navLink = document.querySelectorAll('.nav__link');
+function activeLink() {
+  navLink.forEach((a)=> a.classList.remove('active-link'));
+    this.classList.add('active-link');
+}
+navLink.forEach((a)=>a.addEventListener('click',activeLink));
+
+
+  
 
 /*=============== Mixitup Filter =============== */
 let mixerProjects = mixitup('.projects__container', {
@@ -31,48 +40,4 @@ var  testiSwiper = new Swiper(".testimonial__container", {
     keyboard: true,
   });
 
-  /*=============== Contact Form =============== */
-
-
-  const contactForm = document.getElementById('contact-form'),
-    contactName = document.getElementById('contact-name'),
-    contactEmail = document.getElementById('contact-email'),
-    Message = document.getElementById('message'),
-    contactMessage = document.getElementById('contact-message');
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    // check if the field has a value
-
-    if (
-      contactName.value === '' ||
-      contactEmail.value === '' ||
-      Message.value === ''
-    ) {
-      // add and remove color
-      contactMessage.classList.remove("color-light");
-      contactMessage.classList.add('color-dark');
-
-      //show message 
-      contactMessage.textContent = 'Write all the input fields';
-    }else {
-      // seviceID - templateID - #form - publickey
-      emailjs.sendForm(
-        'service_e6ajfvb',
-        'template_skksui3',
-        '#contact-form',
-        'OxNY-YSN63VeSWqn-'
-      )
-      .then(() => {
-        //show message and add color
-        contactMessage.classList.add('color-light');
-        contactMessage.textContent = 'Message sent✔️'
-
-        //remove message after 5 seconds
-        setTimeout(()=> {
-          contactMessage.textContent = '';
-        },5000)
-      })
-    }
-  };
-  contactForm.addEventListener('submit',sendEmail)
+  
